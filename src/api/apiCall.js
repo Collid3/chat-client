5c61d72f90a994ca592ac3ca45e37462bf512578
Collid3 <masolathabangjerry11@gmail.com>
2023-08-29 11:46:36 +0200

    chage api call base url

--- src/api/apiCall.js
+++ src/api/apiCall.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const protectedApi = axios.create({
-	baseURL: "http://localhost:4000",
-	withCredentials: true,
+  baseURL: "https://seroba-chat-api.onrender.com",
+  withCredentials: true,
 });
 
 export const api = axios.create({
-	baseURL: "http://localhost:4000",
+  baseURL: "https://seroba-chat-api.onrender.com",
 });
