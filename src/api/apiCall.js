0c42607bebe4f1c09a0a7d6cc481ede222b7f474
Collid3 <masolathabangjerry11@gmail.com>
2024-03-12 13:55:24 +0200

    api from dev to production

--- src/api/apiCall.js
+++ src/api/apiCall.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const protectedApi = axios.create({
-  // baseURL: "https://seroba-chat-api.onrender.com",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://seroba-chat-api.onrender.com",
+  // baseURL: "http://localhost:4000",
   withCredentials: true,
 });
 
 export const api = axios.create({
-  // baseURL: "https://seroba-chat-api.onrender.com",
-  baseURL: "http://localhost:4000",
+  baseURL: "https://seroba-chat-api.onrender.com",
+  // baseURL: "http://localhost:4000",
 });
