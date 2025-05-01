d3c50d18d6296c2e0398f81ec0f979327f9ef06a
Thabang Jerry Masola <96353669+Collid3@users.noreply.github.com>
2025-05-01 11:33:04 +0200

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -7,7 +7,7 @@ export const api = axios.create({
   baseURL: `${
     import.meta.env.VITE_ENV === "development"
       ? "http://localhost:5000/api"
-      : "https://chat-server-e9hl.onrender.com"
+      : "https://chat-server-e9hl.onrender.com/api"
   }`,
   withCredentials: true,
 });
